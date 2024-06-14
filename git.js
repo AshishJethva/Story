72191629ff06083370d332d3cd7fe37bf96f4639
Ashish Jethva <jethvaashish2914@gmail.com>
2024-06-14 13:19:20 +0530

    git branch

---
+++ git.js
@@ -0,0 +1,26 @@
+/* Git Commands                       --> Version Control.
+git init
+git status                            --> Files are inside Working Directory but not in "Staging Area".
+git add .                             --> Files "Staging Area" ma aavi jay -> To keep track of changes.
+git commit -m ""                      --> Commit Message.
+git log                               --> To get the record of the commits.
+
+git diff chapter3.txt                 --> To check the Modification in File.
+git checkout chapter3.txt             --> To revert back the changes in file.
+
+***** GitHub *****
+git remote add origin <url>           --> git remote add <name> <url>  // url of the repo.  
+git push -u origin main               --> git push -u <remote name> <branch name>
+
+// .gitignore
+git rm --cached -r .                  --> Badhi files ne staging area mathi remove karva mate.
+
+// Clone
+git clone <url>
+
+// Branch
+git branch <git branch alien-plot>
+git branch                            --> To see all available branches.
+git checkout <alien-plot>             --> To Switch to the branch 'alien-plot'.
+git merge <alien-plot>                --> To merge branch with our main branch.
+*/
